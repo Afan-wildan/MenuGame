8844089f01708fd6434e874e1db43712ab0223fe
widan_ <71113176+Afan-wildan@users.noreply.github.com>
2020-10-09 23:03:15 +0700

    Menu Game
    Tugas5 PGE

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+
+	//waktu delay untuk load scene
+	[SerializeField] private float delayLoading = 3f;
+
+	//nama scene untuk pindah scene yang di inputkan pada inspector
+	[SerializeField] private string nameScene;
+
+	private float timElapsed;
+
+	private void Update () {
+		timElapsed += Time.deltaTime;
+
+		if (timElapsed > delayLoading) {
+		}
+	}
+}
